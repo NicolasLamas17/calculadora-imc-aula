5c176232f5043faeaac97bcefca3e7d5c2ba5a13
Eduardo-DevJs <dudev2001@gmail.com>
2025-06-11 08:02:32 -0300

    aula 2

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Home() {
   return (
@@ -10,17 +16,51 @@ export default function Home() {
           Insira seus dados para calcular seu índice de Massa Corporal. Gênero
         </Text>
 
-        <View>
+        <View style={styles.formulario}>
           <View>
-            <Text></Text>
+            <Text>Altura</Text>
+            <TextInput style={styles.campo} />
           </View>
+
+          <View>
+            <Text>Peso</Text>
+            <TextInput style={styles.campo} />
+          </View>
+
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.texto}>Calcualar IMC</Text>
+          </TouchableOpacity>
         </View>
-        
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1, // ocupar a tela toda
+    justifyContent: "center", // alinhar horizontal centro
+    alignItems: "center", // alinha vertical centro
+    backgroundColor: "#f1f1f1", // pintar o fundo da tela
+    padding: 20, // gordurinha (margem interna)
+  },
+
+  titulo: {
+    fontSize: 23, // tamanho da letra
+    fontWeight: "bold", // peso da fonte (negrito)
+    textAlign: "center", // centralizar o texto
+  },
+
+  caixa: {
+    backgroundColor: "#fff", // branco (cor de fundo)
+    borderRadius: 4, // arredondar as bordas
+    padding: 15, // gordurinha (margem interna)
+  },
+
+  subtitulo: {
+    textAlign: "center", // centraliza o texto
+    color: "greey", // cor do texto
+    fontWeight: "semibold", // peso da fonte (semi negrito)
+    lineHeight: 20, // altura da linha do texto
+  },
 });
